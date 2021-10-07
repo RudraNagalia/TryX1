edb5949339b44fd78fd57dd53da6528d0fe5c8d8
Rudra Nagalia <rudranagalia@gmail.com>
2021-10-06 21:14:21 -0400

    new commit

---
+++ app/src/main/java/com/gmu/tryx1/EmpaticaActivity.java
@@ -0,0 +1,104 @@
+package com.gmu.tryx1;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.empatica.empalink.EmpaDeviceManager;
+import com.empatica.empalink.EmpaticaDevice;
+import com.empatica.empalink.config.EmpaSensorType;
+import com.empatica.empalink.config.EmpaStatus;
+import com.empatica.empalink.delegate.EmpaDataDelegate;
+import com.empatica.empalink.delegate.EmpaStatusDelegate;
+
+public class EmpaticaActivity extends AppCompatActivity implements EmpaDataDelegate, EmpaStatusDelegate {
+    private EmpaDeviceManager deviceManager;
+
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.empatica_activity);
+        deviceManager = new EmpaDeviceManager(getApplicationContext(), this, this);
+        deviceManager.authenticateWithAPIKey("YOUR API KEY");
+    }
+
+    @Override
+    public void didReceiveGSR(float gsr, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveBVP(float bvp, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveIBI(float ibi, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveTemperature(float t, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveAcceleration(int x, int y, int z, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveBatteryLevel(float level, double timestamp) {
+
+    }
+
+    @Override
+    public void didReceiveTag(double timestamp) {
+
+    }
+
+    @Override
+    public void didUpdateStatus(EmpaStatus status) {
+        TextView t = ((TextView) findViewById(R.id.status));
+        t.setText(status.name());
+    }
+
+    @Override
+    public void didEstablishConnection() {
+
+    }
+
+    @Override
+    public void didUpdateSensorStatus(int status, EmpaSensorType type) {
+
+    }
+
+    @Override
+    public void didDiscoverDevice(EmpaticaDevice device, String deviceLabel, int rssi, boolean allowed) {
+
+    }
+
+    @Override
+    public void didFailedScanning(int errorCode) {
+
+    }
+
+    @Override
+    public void didRequestEnableBluetooth() {
+
+    }
+
+    @Override
+    public void bluetoothStateChanged() {
+
+    }
+
+    @Override
+    public void didUpdateOnWristStatus(int status) {
+
+    }
+
+
+}
--- app/src/main/java/com/gmu/tryx1/MainActivity.java
+++ app/src/main/java/com/gmu/tryx1/MainActivity.java
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.empatica.empalink.EmpaDeviceManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent i = new Intent();
-        i.putExtra("thisValue", 3);
-        i.putExtra("thisValue", 7);
-        startActivity(i);
+        Intent myIntent = new Intent(this, EmpaticaActivity.class);
+        startActivity(myIntent);
     }
 }
